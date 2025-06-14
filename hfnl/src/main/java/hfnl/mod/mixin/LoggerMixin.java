b98fcb660c494ca443f7ed6c2ae38aa62bdfe248
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-06-14 11:53:15 +0800

    Logger

--- hfnl/src/main/java/hfnl/mod/mixin/LoggerMixin.java
+++ hfnl/src/main/java/hfnl/mod/mixin/LoggerMixin.java
@@ -1,39 +1,30 @@
 package hfnl.mod.mixin;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.jackhuang.hmcl.Metadata;
 import org.jackhuang.hmcl.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 
+import java.io.*;
 import java.nio.file.Path;
 
 @Mixin(org.jackhuang.hmcl.util.logging.Logger.class)
 public class LoggerMixin {
+    @Shadow @Final public static org.jackhuang.hmcl.util.logging.Logger LOG;
     @Unique
     private static final Logger LOGGER = LoggerFactory.getLogger("HMCL");
 
-    /**
-     * @author
-     * @reason
-     */
     @Overwrite
     private void onExit() {
     }
 
-    /**
-     * @author
-     * @reason
-     */
     @Overwrite
     public void start(Path path) {
     }
-
-    /**
-     * @author
-     * @reason
-     */
 
     @Overwrite
     private void log(Level level, String caller, String msg, Throwable exception) {
@@ -49,4 +40,29 @@ public class LoggerMixin {
             LOGGER.warn(msg, exception);
         }
     }
+
+    @Overwrite
+    public Path getLogFile() {
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        Configuration config = ctx.getConfiguration();
+        FileAppender fileAppender = config.getAppender("File");
+
+        return Path.of(Metadata.CURRENT_DIRECTORY + File.separator + fileAppender.getFileName());
+    }
+
+    @Overwrite
+    public void exportLogs(OutputStream output) throws IOException {
+        String logFilePath = String.valueOf(getLogFile().toAbsolutePath());
+
+        File logFile = new File(logFilePath);
+
+        try (InputStream in = new BufferedInputStream(new FileInputStream(logFile))) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
 }
